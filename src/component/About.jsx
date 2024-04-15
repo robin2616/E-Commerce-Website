58f90975866c16e1a2633b52de55bdf4d5268bc4
robin2616 <robinkumardbg11@gmail.com>
2024-04-15 23:30:08 +0530

    DSF

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -2,9 +2,9 @@ function About(){
     return(<>
 
 
-<div className="h-[470px] w-[700px] bg-blue-200 rounded-xl border-black border-3 translate-x-[300px] translate-y-[20px]">
+{/* <div className="h-[470px] w-[700px] bg-blue-200 rounded-xl border-black border-3 translate-x-[300px] translate-y-[20px]">
 
-</div>
+</div> */}
 
 
 
